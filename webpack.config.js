bb7373d4ae41763a7b931379d083e9a2b5dde158
Daniil Smyshlyaev <smyshlyeavar@gmail.com>
2020-12-17 02:45:02 +0300

    feat: Create project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", 
+  output: {
+    filename: "app.min.js", 
+    path: path.resolve(__dirname, "build"), 
+  },
+  resolve: {
+    extensions: [".js", ".css"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader", 
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin({ filename: "app.min.css" }),
+  ],
+};
